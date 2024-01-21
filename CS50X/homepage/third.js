8416321afe5aac3bbde7a35ecdd39b046fc26fcc
alpha343434 <130374883+alpha343434@users.noreply.github.com>
2024-01-21 14:04:39 +0300

    Create third.js

---
+++ CS50X/homepage/third.js
@@ -0,0 +1,7 @@
+const eliot= document.getElementById("eliot");
+const thirdb= document.getElementById("thirdb");
+
+thirdb.addEventListener("click", function(){
+    const newwindow= window.open("eliot.html","_blank");
+
+});

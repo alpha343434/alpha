5bc3372d3198e044728aa263c2987ae3b2461618
alpha343434 <130374883+alpha343434@users.noreply.github.com>
2024-01-21 14:03:37 +0300

    Create second.js

---
+++ CS50X/homepage/second.js
@@ -0,0 +1,7 @@
+const robert= document.getElementById("robert");
+const secondb= document.getElementById("secondb");
+
+secondb.addEventListener("click", function(){
+    const newwindow= window.open("robert.html","_blank");
+
+});

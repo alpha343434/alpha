f37cf1ffc2b54f93dc34ffda8df349116351451c
alpha343434 <130374883+alpha343434@users.noreply.github.com>
2024-01-21 14:02:41 +0300

    Create first.js

---
+++ CS50X/homepage/first.js
@@ -0,0 +1,7 @@
+const cindy= document.getElementById("cindy");
+const firstb= document.getElementById("firstb");
+
+firstb.addEventListener("click", function(){
+    const newwindow= window.open("cindy.html","_blank");
+
+});

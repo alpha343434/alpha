e4dc549c7a677f59e61f5756e0da18b5e4f13dcf
alpha343434 <130374883+alpha343434@users.noreply.github.com>
2024-03-16 11:42:02 +0300

    Update and rename labweek8..c to arrays.c

--- C/arrays.c
+++ C/arrays.c
@@ -4,24 +4,24 @@
 
 int main(){
 	
-	int dizi[10000];
+	int arr[10000];
 	int i,k;
 	srand(time(NULL));
 	
 	for(i=0;i<10;i++){
 		k = rand() % 100 - 50;
-		dizi[i] = k;
+		arr[i] = k;
 	}
 	for(i=0;i<10;i++){
-		printf("%d. element= %d\n ", i,dizi[i]);
+		printf("%d. element= %d\n ", i,arr[i]);
 	}
 		
 	for(i=0;i<10;i++){
-	if(dizi[i]<0){
-		printf("negative %d\n ",dizi[i]);
+	if(arr[i]<0){
+		printf("negative %d\n ",arr[i]);
 	}
 	else{
-		printf("positive %d\n ",dizi[i]);
+		printf("positive %d\n ",arr[i]);
 	}
 }
 	return 0;

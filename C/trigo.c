a3554b691e2b4492f79701191a61f019bf76d64c
alpha343434 <130374883+alpha343434@users.noreply.github.com>
2024-03-16 11:20:32 +0300

    Update and rename dersten2.c to trigo.c

--- C/trigo.c
+++ C/trigo.c
@@ -5,12 +5,17 @@
 
 int main(){
 	float a,b=180,b2=165,c=115,c2,A=120,A2=100,B,B2,C,C2;
+	
 	a=sqrt(b*b+c*c-2*b*c*cos(A*M_PI/180));
 	printf("a= %f  ",a);
+	
 	B2=asin(b2*sin(A2*M_PI/180)/a);
 	printf("B2= %f ",B2);
+	
 	C2=180-(A2+B2*180/M_PI);
 	printf("C2= %f",C2);
-	
+
+	//atan2(y, x): Calculates the arc tangent of y divided by x, considering the signs of both arguments to determine the quadrant of the result. Returns an angle in radians between -PI and PI.
+	//atan(x): Calculates the arc tangent of a value x, returning the angle in radians between -PI/2 and PI/2.
 	return 0;
 }

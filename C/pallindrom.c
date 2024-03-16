8e62613025d9843fd02d7e6f481296f10f65b2d6
alpha343434 <130374883+alpha343434@users.noreply.github.com>
2024-03-16 11:51:13 +0300

    Update pallindrom.c

--- C/pallindrom.c
+++ C/pallindrom.c
@@ -1,14 +1,30 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
+bool isPalindrome(char *str) {
+    int left = 0;
+    int right = strlen(str) - 1;
 
-int main(){
-	char x[100];
-	gets(x);
-	
-	char *p1,*p2;
-	
-	
-	
-	return 0;
+    while (right > left) {
+        if (str[left++] != str[right--]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    char str[100];
+    
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    if (isPalindrome(str)) {
+        printf("%s is a palindrome.\n", str);
+    } else {
+        printf("%s is not a palindrome.\n", str);
+    }
+
+    return 0;
 }

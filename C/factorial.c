8bed86b9bc3afed7333c323ca30a7cddcc580b4a
alpha343434 <130374883+alpha343434@users.noreply.github.com>
2024-03-16 11:12:21 +0300

    Update and rename bölüm7.c to factorial.c

--- C/factorial.c
+++ C/factorial.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 
 
-int fonksiyon(int a){
+int function(int a){
 	int answer=1,i;
 	for(i=1;i<=a;i++){
 		answer*=i;
@@ -14,7 +14,7 @@ int main(){
 	
 	int x;
 	scanf("%d",&x);
-	printf("%d",fonksiyon(x));
+	printf("%d",function(x));
 	
 	return 0 ;
 }

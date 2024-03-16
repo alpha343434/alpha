63150c6cdd5c2f1dfa18d213bbdd51ef50d15ce8
alpha343434 <130374883+alpha343434@users.noreply.github.com>
2024-03-16 11:46:37 +0300

    Update and rename myc3.c to matrix.c

--- C/matrix.c
+++ C/matrix.c
@@ -3,23 +3,23 @@
 
 int main(){
 	
-	int matris[10][10];
+	int matrix[10][10];
 	int i,j;
 	
 	for(i=0;i<10;i++){
 		for(j=0;j<10;j++){
 			if(i==j){
-				matris[i][j]=1;
+				matrix[i][j]=1;
 			}
 			else{
-				matris[i][j]=0;
+				matrix[i][j]=0;
 			}
 		}
 	}
 	
 	for(i=0;i<10;i++){
 		for(j=0;j<10;j++){
-			printf("%4d",matris[i][j]);
+			printf("%4d",matrix[i][j]);
 		
 		}printf("\n");
 	}

e59a23b9bd4b538871bdffd0858b9572072c2244
alpha343434 <130374883+alpha343434@users.noreply.github.com>
2024-03-16 10:51:00 +0300

    Update mycccccc.c

--- C/mycccccc.c
+++ C/mycccccc.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
 
 int main(){
 	
-	printf("%d\t %d\t %d\t",'a','b','c'); //ASCI tablo deðeerleri
+	printf("%d\t %d\t %d\t",'a','b','c'); //ASCI table values
 	int i;
 	for(i='a';i<='z';i++){
 		printf("%d\n",i);

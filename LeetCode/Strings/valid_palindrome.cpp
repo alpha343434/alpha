bc02cb0269527492e92d0ef0fb4395a08b199a3e
Zehra <130374883+alpha343434@users.noreply.github.com>
2024-04-07 10:53:30 +0300

    Update valid_palindrome.cpp

--- LeetCode/Strings/valid_palindrome.cpp
+++ LeetCode/Strings/valid_palindrome.cpp
@@ -16,3 +16,31 @@ public:
         return true;
     }
 };
+
+// differant approach with using stack:
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        stack <char> s2;
+        int i;
+        std::transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+        s.erase(std::remove_if(s.begin(), s.end(), 
+        []( auto const& c ) -> bool { return !std::isalnum(c); } ), s.end());
+
+        if(s.empty() || s.size()==1) return true;
+
+        for(i=0; i<s.length()/2; i++)
+            s2.push(s[i]);
+        
+        if(s.length() % 2) i++;
+
+        for(;i<s.length(); i++){
+            if(s[i] != s2.top()){
+                return false;
+            }
+            s2.pop();
+        }
+        return true;
+    }
+};
